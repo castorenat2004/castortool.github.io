b4f00676110dcbf2c0758cf96af3dba47de3841a
castorenat2004 <novadesignmx_@hotmail.com>
2025-05-23 00:23:54 -0600

    Add files via upload

---
+++ menu/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<meta name="viewport" content="width=500, initial-scale=1" />
+</head>
+<body>
+	<header>
+		<div class="header">
+			<div class="logo"><h1>Castor Tool</h1></div>
+				<nav class="menu">
+					<a href="../ccgen" class="ccgen-btn">CCGEN</a>
+					<a href="../email-spoofer" class="mail-btn">EMAIL SPOOFER</a>
+				</nav>	
+			</div>
+	</header>
+	<div class="background"></div>
+	<div class="container">
+		<div class="information">
+			<h1>MENU</h1>
+			<hr>
+		</div>
+		<div class="cards">
+			<div class="card">
+				<a href="../ccgen"><img src="../assets/img/ccgen-img.jpg"></a>
+				<h3>CCGEN</h3>
+				<p>Ir al generador de tarjetas de credito</p>
+			</div>
+			<div class="card">
+				<a href="../email-spoofer"><img src="../assets/img/email-spoofer-img.jpg"></a>
+				<h3>EMAIL</h3>
+				<p>Ir al generador de email spoofer</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
